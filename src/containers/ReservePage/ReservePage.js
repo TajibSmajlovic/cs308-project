a910a1768e7e0df906c036273f4a1394cd6da2d1
TajibSmajlovic <smajlovic.tajib@gmail.com>
2019-05-19 15:18:01 +0200

    adds SeatMap for Cinema

--- src/containers/ReservePage/ReservePage.js
+++ src/containers/ReservePage/ReservePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Modal, Button, Image, Header } from "semantic-ui-react";
+import { Modal, Button, Header } from "semantic-ui-react";
+
+import CinemaSeats from '../SeatsMap/CinemaSeats/CinemaSeats'
 
 export default class ReservePage extends Component {
   render() {
@@ -23,32 +25,40 @@ export default class ReservePage extends Component {
               {this.props.event.description}
             </p>
             {/* <div style={{ width: 150, height: 50, border: "1px solid black" }}> */}
-              <p>On <b>{date}</b> at <b>{this.props.event.time}</b> in <b>{this.props.event.venue}</b></p>        
+              <p>On <b>{date}</b> at <b>{this.props.event.time}</b> in <b>{this.props.event.venue}</b></p>
             {/* </div> */}
             <b>PRICE: 10 BAM (per seat)</b>
           </Modal.Description>
         </Modal.Content>
         <hr />
-        <Modal.Content scrolling>
-          <div style={{ width: "90%", height: 350, border: "1px solid black" }}>
-            Select seats
-          </div>
-          <div
-            style={{
-              width: 150,
-              height: 50,
-              border: "1px solid black",
-              display: "inline-block"
-            }}
-          >
-            prive $$
-          </div>
-          <Button.Group floated="right">
-            <Button content="PayPal" primary />
-            <Button content="Card" secondary />
-            <Button content="Coupon" color="teal" />
-          </Button.Group>
-        </Modal.Content>
+		  <Modal.Content scrolling>
+			  <Header as="h3" textAlign="center" content="SELECT SEATS" />
+			  <div
+				  style={{
+					  width: "70%",
+					  border: "1px solid black",
+					  margin: "auto",
+					  padding: 20
+				  }}
+			  >
+				  <CinemaSeats />
+			  </div>
+			  <div
+				  style={{
+					  width: 150,
+					  height: 50,
+					  border: "1px solid black",
+					  display: "inline-block"
+				  }}
+			  >
+				  prive $$
+			  </div>
+			  <Button.Group floated="right">
+				  <Button content="PayPal" primary />
+				  <Button content="Card" secondary />
+				  <Button content="Coupon" color="teal" />
+			  </Button.Group>
+		  </Modal.Content>
       </Modal>
     );
   }
---
+++ src/containers/SeatsMap/TheatreSeats/TheatreSeats.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class TheatreSeats extends React.Component {
+  render() {
+    return;
+  }
+}
